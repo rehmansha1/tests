9bf845cdc4beb151477d06808328db5f7aca53f6
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2024-07-12 13:43:59 +0530

    updated s.js

--- s.js
+++ s.js
@@ -15,7 +15,26 @@ app.options('*', cors(corsOptions));
 
 const PORT = 3002;
 app.get('/',async(req,res)=>{
-  res.send(':-=)')
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.serializd.com/show/Kaiju-No.-8-207468');
+  await page.waitForSelector('.col-md-12.col-12');
+  const t = await page.$$eval('.col-md-12.col-12 > div > div', elements => {
+    return elements.map(element => element.textContent);
+  });
+  var arr = []
+  for (let i = 0; i<t.length; i++){
+    if (t[i].includes('2024')){
+    arr.push(`"${t[i].split('2024')[1]}"`)
+    }
+    else{
+      continue
+    }
+    
+  }
+  res.status(200).send(arr);
+  await browser.close()
 })
 app.get("/getall", async (req, res) => {
 
